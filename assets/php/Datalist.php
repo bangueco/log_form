d9a82cd602418cce2c976490ec2b1e27be2a16a4
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-03-28 20:47:03 +0800

    Rows counter

--- assets/php/Datalist.php
+++ assets/php/Datalist.php
@@ -12,7 +12,18 @@
     <table border="1">
         <thead>
             <tr>
-                <th>Number Of Row/s: 100</th>
+                <th>Number Of Row/s: 
+                    <?php 
+                        $database_connection = new PDO('mysql:host=localhost;dbname=e_logs', 'admin', 'admin');
+                        $stmt = $database_connection->prepare('SELECT * FROM logs');
+                        $stmt->execute();
+                        $log_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                        $rowcount = 0;
+                        foreach($log_data as $logs) {
+                            $rowcount++;
+                        }
+                        echo '<span>'.$rowcount.'</span>';
+                    ?></th>
                 <th>Name</th>
                 <th>Transaction Type</th>
                 <th>Timestamp</th>
