54d7abbe40a9703c4374eea4b859d721b859a057
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-04-01 07:19:35 +0800

    Link fonts

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <title>Prelim Exam - Ivan Gueco</title>
 
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="center">
